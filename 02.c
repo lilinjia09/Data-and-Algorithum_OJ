9cbafb5e7a6714a0d4f635673c05bf6bac121667
李霖嘉 <2927146236@qq.com>
2025-11-07 21:22:47 +0800

    Create 02.c
    开始oj2，第一次提交
    #	状态	时间	内存
    1	Accepted	0 ms	792 KB
    2	Accepted	0 ms	760 KB
    3	Accepted	4 ms	864 KB
    4	Accepted	100 ms	4556 KB
    5	Accepted	84 ms	4560 KB
    6	Memory Limit Exceeded	-	-
    7	Memory Limit Exceeded	-	-
    8	Accepted	0 ms	820 KB
    9	Accepted	0 ms	820 KB
    10	Accepted	0 ms	820 KB
    11	Accepted	44 ms	2712 KB
    12	Accepted	44 ms	2708 KB
    13	Accepted	44 ms	2712 KB
    14	Memory Limit Exceeded	-	-
    15	Accepted	100 ms	4556 KB

---
+++ 02.c
@@ -0,0 +1,35 @@
+//思路：存树？能存吗，还是要边读边调整；找路。线性结构存储树，则第i个元素的父节点为(i-1)/2，左子节点为2*i+1，右子节点为2*i+2,节点数量为 n = (1 << h) - 1
+//找路思路：从根节点开始找左右子树的最大路，然后组合，由于递归，所以出栈时应该是从叶节点出栈，此时开始更新最优值（忽略-99）
+#include <stdio.h>
+#include <stdlib.h>
+void StoreTree(int *tree, int n)
+{
+    for (int i = 0; i < n; i++)
+        scanf("%d", &tree[i]);
+}
+int FindMaxValue(int *tree,int n,int index,int *value)
+{
+    int left=0,right=0;
+    if(index >= n || tree[index]==-99)
+    return 0;
+    left=FindMaxValue(tree,n,2*index+1,value);
+    right=FindMaxValue(tree,n,2*index+2,value);
+    int current=left+right+tree[index];
+    if(current>*value)
+    *value=current;
+    return left>right?left+tree[index]:right+tree[index];
+}
+int main()
+{
+    int h,n;
+    int *tree,value;
+    scanf("%d", &h);
+    n=(1<<h)-1;
+    tree=(int *)malloc(n*sizeof(int));
+    StoreTree(tree,n);
+    value=0;
+    FindMaxValue(tree,n,0,&value);
+    printf("%d",value);
+    free(tree);
+    return 0;
+}
