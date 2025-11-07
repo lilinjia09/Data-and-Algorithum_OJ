941fa485944a3efa5b09f003bfc22bebaf545214
李霖嘉 <2927146236@qq.com>
2025-11-08 01:16:00 +0800

    Update 02.c
    #	状态	时间	内存
    1	Accepted	0 ms	756 KB
    2	Accepted	0 ms	760 KB
    3	Accepted	4 ms	828 KB
    4	Accepted	100 ms	1652 KB
    5	Accepted	84 ms	1652 KB
    6	Accepted	168 ms	2712 KB
    7	Accepted	336 ms	4560 KB
    8	Accepted	0 ms	772 KB
    9	Accepted	0 ms	776 KB
    10	Accepted	0 ms	772 KB
    11	Accepted	44 ms	1128 KB
    12	Accepted	44 ms	1128 KB
    13	Accepted	44 ms	1124 KB
    14	Accepted	164 ms	2712 KB
    15	Accepted	96 ms	1656 KB

--- 02.c
+++ 02.c
@@ -1,41 +1,46 @@
 //思路：存树？能存吗，还是要边读边调整；找路。线性结构存储树，则第i个元素的父节点为(i-1)/2，左子节点为2*i+1，右子节点为2*i+2,节点数量为 n = (1 << h) - 1
 //找路思路：从根节点开始找左右子树的最大路，然后组合，由于递归，所以出栈时应该是从叶节点出栈，此时开始更新最优值（忽略-99）
 //提交1：空间超了，需要优化空间，可能得边扫边处理，思考如何解决
-//想到了，存完一层以后就处理一层，然后释放上一层的空间，但会不会因为局部最优解忽略了全局最优解？
+//目前的结论：做不到层序的时候边读边处理，必须先存树。
 
 #include <stdio.h>
 #include <stdlib.h>
-void StoreTree(int *tree, int n)
+void StoreTree(char *tree, int n)
 {
+    short temp;
     for (int i = 0; i < n; i++)
-        scanf("%d", &tree[i]);
+    {
+        scanf("%d", &temp);
+        tree[i]=(char)temp;
+    }
 }
-int FindMaxValue(int *tree,int n,int index,int *value)
+int FindMaxValue(char *tree,int n,int index,int *value)
 {
     int left=0,right=0;
-    if(index >= n || tree[index]==-99)
+    if(index >= n || (int)tree[index]==-99)
     return 0;
     left=FindMaxValue(tree,n,2*index+1,value);
     right=FindMaxValue(tree,n,2*index+2,value);
     int left_contrib =left>0?left:0;
     int right_contrib=right>0?right:0;
-    int current=left_contrib+right_contrib+tree[index];
+    int current=left_contrib+right_contrib+(int)tree[index];
     if(current>*value)
     *value=current;
     if(left_contrib>=right_contrib)
-    return left_contrib+tree[index];
+    return left_contrib+(int)tree[index];
     else
-    return right_contrib+tree[index];
+    return right_contrib+(int)tree[index];
 }
 int main()
 {
     int h,n;
-    int *tree,value;
+    int value;
+    char *tree;
     scanf("%d", &h);
     n=(1<<h)-1;
-    tree=(int *)malloc(n*sizeof(int));
+    tree=(char *)malloc(n*sizeof(char));
     StoreTree(tree,n);
-    value=0;
+    value=-20;
     FindMaxValue(tree,n,0,&value);
     printf("%d",value);
     free(tree);
